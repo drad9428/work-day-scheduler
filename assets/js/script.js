84d30615f40874510ae367cfd10a32adca3ec24b
drad <robertjdstoddard@gmail.com>
2021-05-29 13:18:48 -0600

    color time accuracy

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,26 @@
 var currentDayEl = $("#currentDay");
+var containerEl = $(".time-block");
 
 currentDayEl.append(moment().format("dddd") + ", " + moment().format("MMM Do"))
+
+var auditSchedule = function(timeblockEl){
+    var hour = $(timeblockEl).find("span").text().trim();
+    var time = moment(hour, "LT")
+    console.log(time, "hour")
+
+    $(timeblockEl).removeClass("past present future")
+
+    if(Math.ceil(moment().diff(time, "hour")) > 0){
+        $(timeblockEl).find("p").addClass("past")
+    }
+    else if(Math.ceil(moment().isSame(time, "hour"))){
+        $(timeblockEl).find("p").addClass("present")
+    }
+    else{
+        $(timeblockEl).find("p").addClass("future")
+    }
+}
+
+for(var i = 0; i < containerEl.length; i++){
+    auditSchedule(containerEl[i])
+}
